1f46f071a8ff499c5db9120cbf2d3bee400f74ca
error-driven-dev <errordrivendev@gmail.com>
2018-04-05 23:39:02 -0700

    Add project files.

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("projects")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
